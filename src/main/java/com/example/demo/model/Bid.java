942c1316faeea2b8ffc19a8d347fca0d0e9808ab
pirana1996 <pirana_coder@hotmail.com>
2018-01-27 01:49:13 +0100

    Basic structure updated.

---
+++ src/main/java/com/example/demo/model/Bid.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.sql.Timestamp;
+
+public class Bid {
+    int bidId;
+    int bidderId;
+    int productId;
+    int amount;
+    Timestamp dateTime;
+}
